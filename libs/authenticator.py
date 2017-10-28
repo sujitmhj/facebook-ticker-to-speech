bfec8fc798628aa6b35b76d1729d341683557541
Sujit Maharjan <sujitmhj@gmail.com>
2017-10-28 13:40:49 +0545

    added authenticator class

---
+++ libs/authenticator.py
@@ -0,0 +1,7 @@
+class Authenticate(object):
+	"""docstring for Authenticate"""
+	def __init__(self, username, password):
+		super(Authenticate, self).__init__()
+		self.username = username
+		self.password = password
+		
